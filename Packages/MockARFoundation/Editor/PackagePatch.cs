5cdd7134f64555d4dc80bb201bcea45602ae8776
Koki Ibukuro <koki.ibukuro@gmail.com>
2021-04-02 03:15:32 +0200

    Apply patch file in C#

--- Packages/MockARFoundation/Editor/PackagePatch.cs
+++ Packages/MockARFoundation/Editor/PackagePatch.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using UnityEngine;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEditor;
-
 using Debug = UnityEngine.Debug;
 
 namespace MockARFoundation
@@ -15,45 +16,49 @@ namespace MockARFoundation
         {
             string packagePath = Path.GetFullPath("Packages/com.unity.xr.arfoundation/Runtime/AR/");
             string toolsPath = Path.GetFullPath("Packages/com.github.asus4.mock-arfoundation/Tools/");
-            string scriptPath = Path.GetFullPath("Packages/com.github.asus4.mock-arfoundation/Tools/apply_patch.sh");
-
-            RunCommand(
-                scriptPath,
+            ApplyPatch(
                 Path.Combine(packagePath, "ARCameraBackground.cs"),
                 Path.Combine(toolsPath, "ARCameraBackground.cs.patch")
             );
         }
 
-        private static bool RunCommand(string scriptPath, string filePath, string patchPath)
+        private static bool ApplyPatch(string filePath, string patchPath)
         {
-            Debug.Assert(File.Exists(scriptPath), $"File: {scriptPath} not found");
             Debug.Assert(File.Exists(filePath), $"File: {filePath} not found");
             Debug.Assert(File.Exists(patchPath), $"File: {patchPath} not found");
 
-            var info = new ProcessStartInfo()
+            var (originalText, replacingText) = LoadPatch(patchPath);
+            // Debug.Log($"original:\n {originalText}");
+            // Debug.Log($"replace with:\n {replacingText}");
+
+            var source = File.ReadAllText(filePath);
+            if (!source.Contains(originalText))
             {
-                FileName = "/bin/bash",
-                WorkingDirectory = Environment.CurrentDirectory,
-                Arguments = $"{scriptPath} {filePath} {patchPath}",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-            };
-            var process = Process.Start(info);
-            var stdout = process.StandardOutput.ReadToEnd();
-            var stderr = process.StandardError.ReadToEnd();
-            process.WaitForExit();
-
-            // if (!string.IsNullOrWhiteSpace(stdout))
-            // {
-            //     Debug.Log($"stdout: {stdout}");
-            // }
-            // if (!string.IsNullOrWhiteSpace(stderr))
-            // {
-            //     Debug.LogError($"stderr: {stderr}");
-            // }
-
+                return false;
+            }
+            Debug.Log($"A patch applyed : {filePath}");
+            var newSource = source.Replace(originalText, replacingText);
+            // Debug.Assert(source != newSource);
+            File.WriteAllText(filePath, newSource);
             return true;
+        }
+
+        private static Tuple<string, string> LoadPatch(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath)
+                .SkipWhile(line => !line.StartsWith("@@"))
+                .Skip(1);
+
+            var originals = lines
+                .Where(line => !line.StartsWith("+"))
+                .Select(line => line.Substring(1));
+
+            var replaceWith = lines
+                .Where(line => !line.StartsWith("-"))
+                .Select(line => line.Substring(1));
+
+            const string separator = "\n";
+            return new Tuple<string, string>(string.Join(separator, originals), string.Join(separator, replaceWith));
         }
 
     }
